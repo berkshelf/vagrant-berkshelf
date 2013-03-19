e0e6837c4b5e7a0bc80a1456a6450ca9f6c5f4b4
Jamie Winsor <jamie@vialstudios.com>
2013-03-19 16:48:05 -0700

    add function documentation

--- lib/berkshelf/vagrant/env.rb
+++ lib/berkshelf/vagrant/env.rb
@@ -1,8 +1,15 @@
 module Berkshelf::Vagrant
+  # @author Jamie Winsor <reset@riotgames.com>
+  #
+  # Environment data to build up and persist through the middleware chain
   class Env
+    # @return [Vagrant::UI::Colored]
     attr_accessor :ui
+    # @return [Berkshelf::Berksfile]
     attr_accessor :berksfile
+    # @return [String]
     attr_accessor :shelf
+    # @return [Berkshelf::Config]
     attr_accessor :config
 
     def initialize
--- lib/berkshelf/vagrant/env_helpers.rb
+++ lib/berkshelf/vagrant/env_helpers.rb
@@ -1,12 +1,22 @@
 module Berkshelf::Vagrant
+  # @author Jamie Winsor <reset@riotgames.com>
+  #
+  # A module of common helper functions that can be mixed into Berkshelf::Vagrant actions
   module EnvHelpers
+    # A file to persist berkshelf-vagrant specific information in between
+    # Vagrant runs.
+    #
     # @return [String]
     def cache_file
       File.join('.vagrant', 'berkshelf')
     end
 
+    # Filter all of the provisioners of the given vagrant environment with the given name
+    #
     # @param [Symbol] name
+    #   name of provisioner to filter
     # @param [Vagrant::Environment, Hash] env
+    #   environment to inspect
     #
     # @return [Array]
     def provisioners(name, env)
@@ -15,8 +25,7 @@ module Berkshelf::Vagrant
       config_global.vm.provisioners.select { |prov| prov.name == name }
     end
 
-    # Determine if the given instance of Vagrant::Config::Top contains a
-    # chef_solo provisioner
+    # Determine if the given vagrant environment contains a chef_solo provisioner
     #
     # @param [Vagrant::Environment] env
     #
@@ -25,8 +34,7 @@ module Berkshelf::Vagrant
       provisioners(:chef_solo, env).any?
     end
 
-    # Determine if the given instance of Vagrant::Config::Top contains a
-    # chef_client provisioner
+    # Determine if the given vagrant environment contains a chef_client provisioner
     #
     # @param [Vagrant::Environment] env
     #
