b995eef21be2ce6f952862df50ceebaff5a580ae
Jamie Winsor <jamie@vialstudios.com>
2013-03-19 16:56:29 -0700

    clean the shelf if it exists no matter what; regardless of provisioner

--- lib/berkshelf/vagrant/action/clean.rb
+++ lib/berkshelf/vagrant/action/clean.rb
@@ -9,7 +9,7 @@ module Berkshelf::Vagrant
       end
 
       def call(env)
-        if chef_solo?(env) && env[:berkshelf].shelf && File.exist?(env[:berkshelf].shelf)
+        if env[:berkshelf].shelf && File.exist?(env[:berkshelf].shelf)
           env[:berkshelf].ui.info "Cleaning Vagrant's berkshelf"
 
           FileUtils.remove_dir(env[:berkshelf].shelf, force: true)
