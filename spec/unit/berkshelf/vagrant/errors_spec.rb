4409cdf3d8cb29c3b7829daed05f4649cc2579b1
Jamie Winsor <jamie@vialstudios.com>
2013-03-19 17:18:33 -0700

    add test for VagrantWrapperError

---
+++ spec/unit/berkshelf/vagrant/errors_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Berkshelf::VagrantWrapperError do
+  subject { described_class }
+
+  it "proxies messages to the original exception" do
+    original = double('original_error')
+    original.should_receive(:a_message)
+
+    subject.new(original).a_message
+  end
+end
