16bf87b6a1860e17d8558e684e5c69a3b10478ed
Jamie Winsor <jamie@vialstudios.com>
2013-07-15 22:05:39 -0700

    use Berksfile#vendor instead of Berksfile#install with :path option

--- lib/berkshelf/vagrant/action/load_shelf.rb
+++ lib/berkshelf/vagrant/action/load_shelf.rb
@@ -15,9 +15,11 @@ module Berkshelf
           end
 
           # Make sure that Berkshelf itself uses distinct directories for each vagrant run.
-          ENV['BERKSHELF_PATH'] = File.join(Berkshelf.berkshelf_path, env[:machine].name.to_s) unless ENV['BERKSHELF_PATH']
+          unless ENV['BERKSHELF_PATH']
+            ENV['BERKSHELF_PATH'] = File.join(Berkshelf.berkshelf_path, env[:machine].name.to_s)
+          end
 
-          shelf = load_shelf env
+          shelf = load_shelf(env)
 
           if shelf.nil?
             shelf = cache_shelf(Berkshelf::Vagrant.mkshelf(env[:machine].name), env)
